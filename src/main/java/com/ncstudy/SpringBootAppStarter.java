12a51d0ea2f80f3e03643df5ab525ff1476feb61
God <2425990981@qq.com>
2024-10-11 16:51:23 +0800

    学习中4

--- src/main/java/com/ncstudy/SpringBootAppStarter.java
+++ src/main/java/com/ncstudy/SpringBootAppStarter.java
@@ -6,8 +6,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @MapperScan("com.ncstudy.mapper")  //扫描的mapper,因为mapper是mybatis的, 而@Repposity是spring自带的
@@ -16,7 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
                                    //(exclude={DruidDataSourceAutoConfigure.class,DataSourceAutoConfiguration.class})
 @ServletComponentScan("com.ncstudy.config.servletconfig")
 @EnableConfigurationProperties     //使@ConfigurationProperties(prefix = "xxx")注解的类生效
-public class SpringBootAppStarter {
+public class SpringBootAppStarter {// extends SpringBootServletInitializer {
   public static void main( String[] args ) {
 
       SpringApplication springApplication = new SpringApplication(SpringBootAppStarter.class);
@@ -25,4 +27,9 @@ public class SpringBootAppStarter {
       springApplication.run(args);
      // SpringApplication.run(SpringBootAppStarter.class, args);
   }
+
+    // @Override
+    // protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    //     return builder.sources(this.getClass());
+    // }
 }

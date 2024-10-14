26a14cc8d61aa40fcb4e66618f29e6b36d8fd8cf
God <2425990981@qq.com>
2024-10-14 16:21:50 +0800

    学习中5  筛选2

--- src/main/java/com/ncstudy/SpringBootAppStarter.java
+++ src/main/java/com/ncstudy/SpringBootAppStarter.java
@@ -1,15 +1,11 @@
 package com.ncstudy;
 
-import com.ncstudy.listener.ApplicationPreparedListener;
-import com.ncstudy.listener.ContextStoppedEventListener;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @MapperScan("com.ncstudy.mapper")  //扫描的mapper,因为mapper是mybatis的, 而@Repposity是spring自带的
@@ -18,18 +14,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
                                    //(exclude={DruidDataSourceAutoConfigure.class,DataSourceAutoConfiguration.class})
 @ServletComponentScan("com.ncstudy.config.servletconfig")
 @EnableConfigurationProperties     //使@ConfigurationProperties(prefix = "xxx")注解的类生效
-public class SpringBootAppStarter  extends SpringBootServletInitializer {
-  public static void main( String[] args ) {
-
+public class SpringBootAppStarter { //extends SpringBootServletInitializer {
+  public static void main( String[] args )
+  {
       SpringApplication springApplication = new SpringApplication(SpringBootAppStarter.class);
-     // springApplication.addListeners(new ApplicationPreparedListener());
-     // springApplication.addListeners(new ContextStoppedEventListener());
       springApplication.run(args);
-     // SpringApplication.run(SpringBootAppStarter.class, args);
   }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(this.getClass());
-    }
+    // @Override
+    // protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    //     return builder.sources(this.getClass());
+    // }
 }
